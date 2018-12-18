c1c36ae08d02a2f04d0ec24aba44a82960ac3258
김창현 <kbspkpk@naver.com>
2018-12-17 21:12:30 -0800

    Initial Commit

---
+++ SandolTests/SandolTests.swift
@@ -0,0 +1,34 @@
+//
+//  SandolTests.swift
+//  SandolTests
+//
+//  Created by 김창현 on 12/17/18.
+//  Copyright © 2018 Albatross. All rights reserved.
+//
+
+import XCTest
+@testable import Sandol
+
+class SandolTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
